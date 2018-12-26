77f1fb7a4270f2423237fb7ac99861b73c6d8d75
Luana-silva <luana-silva@gmail.com>
2018-12-26 18:30:11 -0200

    Constants

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -6,8 +6,8 @@ export class Constants {
   //public static get SERVICE_URL(): string { return "http://dev.mangobits.net"; };
 
   //10
-   public static get SERVICE_URL(): string { return "http://192.168.123.10:8080"; };
-  // public static get SERVICE_URL(): string { return "http://mangobits.servebeer.com:8080"; };
+  // public static get SERVICE_URL(): string { return "http://192.168.123.10:8080"; };
+  public static get SERVICE_URL(): string { return "http://mangobits.servebeer.com:8080"; };
 
   //Desenvolvimeno
   // public static get SERVICE_URL(): string { return "http://127.0.0.1:8080"; };
