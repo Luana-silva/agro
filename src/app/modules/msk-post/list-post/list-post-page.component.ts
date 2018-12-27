ba37f56849304a8ebcc8653c27732494553c969c
Carlos Alito <carlos.alito@doxti.com.br>
2018-12-26 23:43:17 -0200

    datatable pagination

--- src/app/modules/msk-post/list-post/list-post-page.component.ts
+++ src/app/modules/msk-post/list-post/list-post-page.component.ts
@@ -13,7 +13,9 @@ declare var require: any;
 @Component({
   selector: 'app-list-post-page',
   templateUrl: './list-post-page.component.html',
-  styleUrls: ['./list-post-page.component.scss'],
+  styleUrls: ['./list-post-page.component.scss',
+  '../../../../assets/fonts/icofont.css',
+  ],
   providers: [
     PostService,
     DateFormatPipe3,
@@ -26,12 +28,17 @@ export class ListPostPageComponent implements OnInit {
 
   post: Post;
 
-  rows = [];
+  rows = {};
+  rowsPage = [];
+  page = {
+    size: 10,
+    totalElements: 0,
+    totalPages: 0,
+    pageNumber: 1
+  }
 
-  temp = [];
-
-  loadingIndicator: boolean = true;
-  reorderable: boolean = true;
+  loadingIndicator = true;
+  reorderable = true;
 
   url: string[];
   urlApprove: boolean;
@@ -74,12 +81,7 @@ export class ListPostPageComponent implements OnInit {
 
   ngOnInit() {
 
-    this.postService.listPending({page: 1}, (result, list) => {
-      if (list != null) {
-        this.rows = list['listPosts'];
-        this.temp = [...list['listPosts']];
-      }
-    });
+    this.selectTable();
 
     if (!this.urlApprove) {
       this.navBarDataService.changePageTitle('Notícias')
@@ -87,6 +89,24 @@ export class ListPostPageComponent implements OnInit {
       this.navBarDataService.changePageTitle('Notícias')
     }
 
+  }
+
+  selectTable() {
+    this.postService.listPending({page: this.page.pageNumber}, (result, list: any) => {
+      console.log(result, list);
+      if (list != null) {
+        if (this.page.pageNumber === 1) {
+          // pego aqui o total apenas umas vez...
+          // nesse caso a api tbm só precisaria pegar o total na 1a vez, consumindo menos banda e diminuindo o overhead da chamada
+
+          this.page.totalElements = list.totalAmount;
+          this.page.totalPages = list.pageQuantity;
+        }
+
+        this.rows[this.page.pageNumber] = list.listPosts;
+        this.rowsPage = list.listPosts;
+      }
+    });
   }
 
   view(row: any) {
@@ -106,7 +126,7 @@ export class ListPostPageComponent implements OnInit {
     const val = event.target.value.toLowerCase();
 
     // filter our data
-    const temp = this.temp.filter(function (data) {
+    const temp = this.rowsPage.filter(function (data) {
 
       let creationDate = new DateFormatPipe3('en-US').transform(data.creationDate);
       return (data.data.toLowerCase().indexOf(val) !== -1) || (creationDate.toString().includes(val) || !val);
@@ -116,6 +136,16 @@ export class ListPostPageComponent implements OnInit {
     this.rows = temp;
     // Whenever the filter changes, always go back to the first page
     this.table.offset = 0;
+  }
+
+  setPage(pageInfo) {
+    // esse +1 é devido sua api iniciar em 1 e não em 0...
+    this.page.pageNumber = pageInfo.offset + 1;
+    if (this.rows[this.page.pageNumber]) {
+      this.rowsPage = this.rows[this.page.pageNumber]
+    } else {
+      this.selectTable();
+    }
   }
 
 }
